a6fcb71c695b5c91ea483ae888bc716159209468
GroupPluginer <4gamesl7o@gmail.com>
2017-07-22 19:23:18 +0300

    Create Main.php

---
+++ src/con/Main.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace con;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\utils\Config;
+use pocketmine\command\CommandSender;
+use pocketmine\command\Command;
+
+class Main extends PluginBase{
+
+ public function onEnable(){
+  $this->getServer()->getLogger()->info("config test enabled");
+  @mkdir->($this-<getDatatFolder());
+  $m = [
+   'm' => 'here'
+   ];
+   
